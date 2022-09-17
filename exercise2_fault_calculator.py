0bdbb5d169bc1faf9bdc313a7f4fdef76db23d0c
Rinku05-2002 <113429124+Rinku05-2002@users.noreply.github.com>
2022-09-17 10:04:07 +0530

    Update exercise2_fault_calculator.py

--- exercise2_fault_calculator.py
+++ exercise2_fault_calculator.py
@@ -1,13 +1,3 @@
-
-
-
-
-
-                                                #SECOND METHOD#
-
-
-
-
 num1=int(input("enter the first number: "))
 num2=int(input("enter the second number: "))
 operator=(input("enter the sign of opration: "))
